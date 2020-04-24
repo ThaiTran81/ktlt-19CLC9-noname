621b1040b314e4ba17da44177db531110934fcc1
ThaiTran81 <63252276+ThaiTran81@users.noreply.github.com>
2020-04-24 19:50:55 +0700

    add functions, student.txt and update data files

--- CS162-Final Project/CS162-Final Project/Main.cpp
+++ CS162-Final Project/CS162-Final Project/Main.cpp
@@ -1,4 +1,14 @@
-#include<iostream>
-int main() {
+#include"function.h"
+
+int main()
+{
+	ifstream fi;
+	ofstream fo;
+	int nstudent, nstaff;
+	Student* arrStu;
+	Staff* arrSta;
+	char* userid;
+	char* userpwd;
+
 	return 0;
 }
---
+++ CS162-Final Project/CS162-Final Project/function.h
@@ -0,0 +1,65 @@
+#ifndef _FUNCTION_H_
+#define _FUNCTION_H_
+
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<string>
+using namespace std;
+
+
+struct nodeStu {
+	Student data;
+	nodeStu* next;
+};
+
+struct LinkedListStu {
+	nodeStu* head;
+};
+
+struct nodeSta {
+	Staff data;
+	nodeSta* next;
+};
+
+struct LinkedListSta {
+	nodeSta* head;
+};
+
+struct Staff {
+	char* id;
+	char* password;
+	char* fullname;
+	int typeMenu;
+	bool sex;
+};
+
+struct Student {
+	char id[10];
+	char* password;
+	char* fullname;
+	char* classId;
+
+	int day;
+	int month;
+	int year;
+
+	int typeMenu;
+	bool status;
+	bool sex;
+};
+
+
+void Login(char* userid, char* userpwd);
+bool LoadDataStaff(ifstream& fi, Staff*& arr);
+bool LoadDataStudent(ifstream& fi, Student*& arr);
+bool SaveDataStaff(ofstream& fo, int& nstaff, Staff* arr);
+bool SaveDataStudent(ofstream& fo, int& nstudent, Student* arr);
+void ViewProfileStaff();
+void MenuStaff();///type menu =1
+void MenuStudent();//type menu=3
+void ImportStudentScv();
+void EditStudent();
+void RemoveStudent();
+#endif // !_FUNCTION_H_
+
