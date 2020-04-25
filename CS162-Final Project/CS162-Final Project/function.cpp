16a424616b01d76f13001b86149b9feed0406b85
Nguyễn Anh Tuấn <63799789+nguyenanhtuan-19127614@users.noreply.github.com>
2020-04-25 22:18:10 +0700

    Xong hàm save,load staff r nha
    t có chỉnh sửa tí mấy cái struct á

--- CS162-Final Project/CS162-Final Project/function.cpp
+++ CS162-Final Project/CS162-Final Project/function.cpp
@@ -1,1 +1,67 @@
 #include"function.h"
+//             SAVE, LOAD STAFF             //
+nodeSta* createNodeStaff(Staff x)
+{
+	nodeSta* p = new nodeSta;
+	p->dataStaf.id = x.id;
+	p->dataStaf.password = x.password;
+	p->dataStaf.fullname = x.fullname;
+	p->dataStaf.typeMenu = x.typeMenu;
+	p->dataStaf.sex = x.sex;
+	p->next = NULL;
+	return p;
+}
+bool LoadDataStaff(ifstream& fi, LinkedListSta& lst)
+{
+	lst.head = NULL;
+	Staff staf;
+	fi >> lst.NumSta;
+	if (lst.NumSta == 0)
+	{
+		return false;
+	}
+	else
+	{
+		fi >> staf.id;
+		fi >> staf.password;
+		getline(fi, staf.fullname);
+		getline(fi, staf.fullname);
+		fi >> staf.typeMenu;
+		fi >> staf.sex;
+		nodeSta* p = createNodeStaff(staf);
+		lst.head = p;
+		nodeSta* cur = lst.head;
+		for (int i = 0; i < lst.NumSta - 1; i++)
+		{
+			fi >> staf.id;
+			fi >> staf.password;
+			getline(fi, staf.fullname);
+			getline(fi, staf.fullname);
+			fi >> staf.typeMenu;
+			fi >> staf.sex;
+			p = createNodeStaff(staf);
+			cur->next = p;
+			cur = cur->next;
+		}
+		return true;
+	}
+}
+bool SaveDataStaff(ofstream& fo, LinkedListSta& lst)
+{
+	if (lst.head == NULL)
+	{
+		return false;
+	}
+	nodeSta* cur = lst.head;
+	while (cur != NULL)
+	{
+		fo << cur->dataStaf.id << endl;
+		fo << cur->dataStaf.password << endl;
+		fo << cur->dataStaf.fullname << endl;
+		fo << cur->dataStaf.typeMenu << endl;
+		fo << cur->dataStaf.sex << endl;
+		cur = cur->next;
+	}
+	return true;
+}
+//________________________________________________________//
--- CS162-Final Project/CS162-Final Project/function.h
+++ CS162-Final Project/CS162-Final Project/function.h
@@ -6,34 +6,13 @@
 #include<cstring>
 #include<string>
 using namespace std;
-
-
-struct nodeStu {
-	Student data;
-	nodeStu* next;
-};
-
-struct LinkedListStu {
-	nodeStu* head;
-};
-
-struct nodeSta {
-	Staff data;
-	nodeSta* next;
-};
-
-struct LinkedListSta {
-	nodeSta* head;
-};
-
 struct Staff {
-	char* id;
-	char* password;
-	char* fullname;
+	string id;
+	string password;
+	string fullname;
 	int typeMenu;
 	bool sex;
 };
-
 struct Student {
 	char id[10];
 	char* password;
@@ -48,12 +27,29 @@ struct Student {
 	bool status;
 	bool sex;
 };
+struct nodeStu {
+	Student data;
+	nodeStu* next;
+};
 
+struct LinkedListStu {
+	nodeStu* head;
+};
 
+struct nodeSta {
+	Staff dataStaf;
+	nodeSta* next;
+};
+
+struct LinkedListSta {
+	int NumSta;
+	nodeSta* head;
+};
+nodeSta* createNodeStaff(Staff x); // Dung de tao Node cho staff <Done>
 void Login(char* userid, char* userpwd, LinkedListSta& lstSta, LinkedListStu& lstStu);
-bool LoadDataStaff(ifstream& fi, LinkedListSta& lst);
+bool LoadDataStaff(ifstream& fi, LinkedListSta& lst);// Load du lieu txt vao list staff <Done>
 bool LoadDataStudent(ifstream& fi, LinkedListStu& lst);
-bool SaveDataStaff(ofstream& fo, int& nstaff, LinkedListSta& lst);
+bool SaveDataStaff(ofstream& fo, LinkedListSta& lst); // Save du lieu tu list staff ra txt <Done>
 bool SaveDataStudent(ofstream& fo, int& nstudent, LinkedListStu& lst);
 void ViewProfileStaff(LinkedListSta& lst);
 void MenuStaff();///type menu =1
